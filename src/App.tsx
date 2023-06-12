1ba62cb608c5e979267c8fcca68b510104a94cf5
Ian Augusto Silva <ian.silva@N0137391.local>
2023-06-11 23:30:31 -0300

    continuando...

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,40 @@
-import React, { useState } from 'react';
+import "./style.css";
+
+import React, { useState } from "react";
 
 function App() {
-  const [answer, setAnswer] = useState<string>('')
+	const [answer, setAnswer] = useState<string>("");
 
-  const handleYesClick = ():void => {
-    setAnswer("SIM");
-  };
+	const handleYesClick = (): void => {
+		setAnswer("te amo sua linda <3");
+	};
 
-  const handleNoClick= ():void => {
-    setAnswer("NÃO");
-  };
+	const handleNoHover = (): void => {
+		const button = document.querySelector<HTMLButtonElement>("#no-button");
 
-  const handleNoHover = ():void => {
-    const button = document.querySelector<HTMLButtonElement>('#no-button');
-  
-    if (button) {
-      const buttonWidth = button.offsetWidth;
-      const buttonHeight = button.offsetHeight;
-      const screenWidth = window.innerWidth;
-      const screenHeight = window.innerHeight;
-      const randomX = Math.floor(Math.random() * (screenWidth - buttonWidth));
-      const randomY = Math.floor(Math.random() * (screenHeight - buttonHeight));
-      button.style.transform = `translateX(${randomX}px) translateY(${randomY}px)`;
-    }      
-  }
+		if (button) {
+			const buttonWidth = button.offsetWidth;
+			const buttonHeight = button.offsetHeight;
+			const screenWidth = window.innerWidth;
+			const screenHeight = window.innerHeight;
+			const randomX = Math.floor(Math.random() * (screenWidth - buttonWidth));
+			const randomY = Math.floor(Math.random() * (screenHeight - buttonHeight));
+			button.style.transform = `translateX(${randomX}px) translateY(${randomY}px)`;
+		}
+	};
 
-
-  return (
-    <div>
-      <h1>Quer namorar comigo?</h1>
-      <button onClick={handleYesClick}>SIM</button>
-      <button id="no-button" onMouseOver={handleNoHover} onClick={handleNoClick}>
-          Não
-        </button>
-        {answer && <p>Você respondeu: {answer}</p>}
-    </div>
-  )
+	return (
+		<div className='container'>
+			<h1>Quer namorar comigo?</h1>
+			<button className='buttonYes' onClick={handleYesClick}>
+				SIM
+			</button>
+			<button className='buttonNo' id='no-button' onMouseOver={handleNoHover}>
+				NÃO
+			</button>
+			{answer && <p>Você respondeu: {answer}</p>}
+		</div>
+	);
 }
 
 export default App;
